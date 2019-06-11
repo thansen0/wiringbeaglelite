920a9a9fdf5cd9f523449f61fc2f81aa2083daaa
Demo User <debian@beaglebone.localdomain>
2019-06-11 01:16:52 +0000

    Added test case for digitalRead

--- example.c
+++ example.c
@@ -8,19 +8,30 @@
 
 int main() {
 	int i;
-	i = pinMode(67, OUTPUT);
+	int pin = 67;
+	i = pinMode(pin, OUTPUT);
 	
 	printf("pinMode status should be 0 if successful: %i\n", i);
-	
+
 	// turns pin off (sets value to 1)
-	digitalWrite(67, HIGH);
+	digitalWrite(pin, HIGH);
 
 	sleep(3);
 
 	// turns pin off (sets value to 0)
-	digitalWrite(67, LOW);
+	digitalWrite(pin, LOW);
 
 	// optional, undoes pinMode
-	pinMode(67, UNEXPORT);
+	pinMode(pin, UNEXPORT);
 
+	// allow unexporting to process
+	sleep(3);
+
+	pinMode(pin, INPUT);
+
+	int in = digitalRead(pin);
+
+	printf("digitalRead in is 0: %i\n", in);
+
+	pinMode(pin, UNEXPORT);
 }
